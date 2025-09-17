6bfbffc464b0b02a391a7d04b487c5f7436c76ed
precious-israel <carisabiodun@gmail.com>
2025-09-17 14:45:29 +0100

    updated built css

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,9 +2,7 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController")
-// const expressLayouts = require("express-ejs-layouts");
-// require("dotenv").config();
-// const app = express();
+
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
@@ -13,25 +11,5 @@ module.exports = router;
 
 
 
-// const staticRoutes = require("./static");
-// const baseController = require("../controllers/baseController");
 
 
-// // View Engine
-// app.set("view engine", "ejs");
-// app.use(expressLayouts);
-// app.set("layout", "./layouts/layout");
-
-// // Index route
-// app.get("/", baseController.home);
-
-// // Other routes
-// app.use(staticRoutes);
-
-// // Server
-// const port = process.env.PORT || 5500;
-// const host = process.env.HOST || "localhost";
-
-// app.listen(port, () => {
-//   console.log(`App listening on ${host}:${port}`);
-// });
