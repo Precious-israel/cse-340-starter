c104563d46f77ad8fba5cf388457e62d441fff3e
precious-israel <carisabiodun@gmail.com>
2025-10-01 13:42:22 +0100

    update db

--- database/index.js
+++ database/index.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "development" ? { rejectUnauthorized: false } : false,
+  ssl: process.env.NODE_ENV === "production" 
+    ? { rejectUnauthorized: false } 
+    : false,
 });
 
 module.exports = {
@@ -13,7 +15,7 @@ module.exports = {
       console.log("executed query", { text });
       return res;
     } catch (error) {
-      console.error("error in query", { text });
+      console.error("error in query", { text, error: error.message });
       throw error;
     }
   },
